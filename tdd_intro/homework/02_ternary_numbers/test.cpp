5cb4c8d945ce664a795c1067b52c25d993b774ee
Andy Bisikalo <andy@apriorit.com>
2018-10-10 17:01:32 +0300

    green: fake implementation for invalid symbol

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -17,9 +17,14 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
 
-unsigned FromTernaryChar(char symbol)
+int FromTernaryChar(char symbol)
 {
-    unsigned result = static_cast<unsigned>(symbol - '0');
+    if (symbol == '3')
+    {
+        return -1;
+    }
+
+    int result = symbol - '0';
     return result;
 }
 
