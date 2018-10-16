05902e29e2fd1a941943560e6f51ce59b8960605
Andy Bisikalo <andy@apriorit.com>
2018-10-16 20:00:55 +0300

    green: fake GetDigit(index) implementation

--- tdd_intro/homework/03_bank_ocr/test.cpp
+++ tdd_intro/homework/03_bank_ocr/test.cpp
@@ -206,6 +206,14 @@ bool operator==(const Digit& left, const Digit& right)
 
 Digit GetDigit(const Display& display, size_t index)
 {
+    if (display.lines[0] == s_display123456789.lines[0] &&
+            display.lines[1] == s_display123456789.lines[1] &&
+            display.lines[2] == s_display123456789.lines[2] &&
+            index == 4)
+    {
+        return s_digit5;
+    }
+
     return Digit{
         display.lines[0].substr(0, 3),
         display.lines[1].substr(0, 3),
