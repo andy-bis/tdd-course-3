c2bc9b982809cda696dde047f810cf4860e0bdf5
Andy Bisikalo <andy@apriorit.com>
2018-11-07 00:08:32 +0200

    green: faked Big Americano

--- tdd_intro/homework/06_coffee/test.cpp
+++ tdd_intro/homework/06_coffee/test.cpp
@@ -65,9 +65,18 @@ public:
     }
     void CreateCoffee(const Cup cup, const Coffee coffee)
     {
-        m_source.AddCoffee(50);
-        m_source.SetCupSize(100);
-        m_source.AddWater(50, 60);
+        if (cup == Cup::Big)
+        {
+            m_source.AddCoffee(70);
+            m_source.SetCupSize(140);
+            m_source.AddWater(70, 60);
+        }
+        else
+        {
+            m_source.AddCoffee(50);
+            m_source.SetCupSize(100);
+            m_source.AddWater(50, 60);
+        }
     }
 private:
     ISourceOfIngredients& m_source;
